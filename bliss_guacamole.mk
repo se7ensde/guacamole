738f635e078340c092142a21ab833a29ba4c1db5
LuK1337 <priv.luk@gmail.com>
2019-07-03 21:17:38 -0600

    guacamole: Sync lineage_guacamole.mk with pioneer's layout

--- bliss_guacamole.mk
+++ bliss_guacamole.mk
@@ -4,7 +4,14 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from guacamole device
 $(call inherit-product, device/oneplus/guacamole/device.mk)
+
+# Inherit some common Bliss stuff.
 $(call inherit-product, vendor/bliss/config/common.mk)
 
 # Device identifier. This must come after all inclusions.

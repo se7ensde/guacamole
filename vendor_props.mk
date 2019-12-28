0e3483cbd05d143bce5caeb1ad840ecd06a4697e
chandra <chandra.chaganti@hotmail.com>
2019-12-28 08:10:26 +0530

    guacamole: Update haptic feedback intensities

--- vendor_props.mk
+++ vendor_props.mk
@@ -7,3 +7,10 @@
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=560
+
+# Haptic feedback
+PRODUCT_PROPERTY_OVERRIDES += \
+    sys.haptic.down.weak=20 \
+    sys.haptic.down.normal=40 \
+    sys.haptic.down.strong=80 \
+    sys.haptic.runin=13

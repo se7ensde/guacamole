5190011e79e7f5ec233c043b3716fb9c05b1ec55
Glen Kuhne <kuh@google.com>
2020-01-30 14:18:09 -0700

    guacamole: Set default hotspot ssid
    Added a device specific overlay that changes the default name
    from "AndroidAP_*" to "OnePlus 7Pro"

--- device.mk
+++ device.mk
@@ -17,7 +17,7 @@ PRODUCT_CHARACTERISTICS := nosdcard
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-bliss
 
 # Audio
 PRODUCT_COPY_FILES += \

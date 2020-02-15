58800eb4c2b20a61a7793150ef4ec716ca62a9c2
Chandra Chaganti <chandra.chaganti@hotmail.com>
2020-02-15 13:46:59 +0530

    guacamole: Update build fingerprint to Coral's Feb 2020

--- init/init_guacamole.cpp
+++ init/init_guacamole.cpp
@@ -57,5 +57,5 @@ void vendor_load_properties()
 {
     // fingerprint
     property_override("ro.build.description", "OnePlus7Pro-user 10 QKQ1.190716.003 1912141900 release-keys");
-    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ1B.191205.011/5974828:user/release-keys");
+    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ1B.200205.002/6084387:user/release-keys");
 }
